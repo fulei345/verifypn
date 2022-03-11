a1099628cbcac620d0e840ba3792f557cd520649
t-bsh <tbsh18@student.aau.dk>
2022-03-11 11:31:46 +0100

    update SMCSUCC

--- include/SMC/SuccessorGeneration/SMCSuccessorGenerator.h
+++ include/SMC/SuccessorGeneration/SMCSuccessorGenerator.h
@@ -4,16 +4,16 @@ namespace PetriEngine{
     class SMCSuccessorGenerator : public PetriEngine::SuccessorGenerator{
         public:
         SMCSuccessorGenerator(const PetriEngine::PetriNet *net);
+        
+        bool SMCSuccessorGenerator::next(Structures::State& write, uint32_t &tindex) {
+            bool has_suc = PetriEngine::SuccessorGenerator::next(write, tindex);
+            // do thing with tindex?
+            return has_suc;
+        }
+
+        const Structures::State* _parent;
+
+        uint32_t _suc_pcounter;
+        uint32_t _suc_tcounter;
     };
-    public:
-    bool SMCSuccessorGenerator::next(Structures::State& write, uint32_t &tindex) {
-        bool has_suc = PetriEngine::SuccessorGenerator::next(write);
-        // do thing with tindex?
-        return has_suc
-    }
-
-    const Structures::State* _parent;
-
-    uint32_t _suc_pcounter;
-    uint32_t _suc_tcounter;
 }
