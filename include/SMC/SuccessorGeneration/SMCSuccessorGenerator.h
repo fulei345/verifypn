30816aad38de2dad5c7d3a7fde745455302ce712
Fulei <emil.fulei@gmail.com>
2022-03-14 09:05:58 +0100

    h file fixed

--- include/SMC/SuccessorGeneration/SMCSuccessorGenerator.h
+++ include/SMC/SuccessorGeneration/SMCSuccessorGenerator.h
@@ -1,21 +1,21 @@
 #include "PetriEngine/SuccessorGenerator.h"
-#include "PetriEngine/PetriNet.h"
-#include "PetriEngine/Structures/State.h"
 
-namespace SMC{
+namespace PetriEngine{
     class SMCSuccessorGenerator : public PetriEngine::SuccessorGenerator{
         public:
         SMCSuccessorGenerator(const PetriEngine::PetriNet *net);
-        
-        bool SMCSuccessorGenerator::next(Structures::State& write, uint32_t &tindex) {
-            bool has_suc = PetriEngine::SuccessorGenerator::next(write, tindex);
+    
+        public:
+        bool next(Structures::State& write, uint32_t &tindex) {
+            bool has_suc = PetriEngine::SuccessorGenerator::next(write);
             // do thing with tindex?
             return has_suc;
         }
 
-        const Structures::State* _parent;
+    const Structures::State* _parent;
 
-        uint32_t _suc_pcounter;
-        uint32_t _suc_tcounter;
+    uint32_t _suc_pcounter;
+    uint32_t _suc_tcounter;
+
     };
 }
