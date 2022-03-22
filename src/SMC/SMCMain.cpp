e980d0c2d777e3ca98304b502d7a1f458521e476
t-bsh <tbsh18@student.aau.dk>
2022-03-22 12:34:23 +0100

    update print test

--- src/SMC/SMCMain.cpp
+++ src/SMC/SMCMain.cpp
@@ -26,16 +26,25 @@
 using namespace PetriEngine;
 
 namespace SMC {
-    bool SMCRun(SMCSuccessorGenerator& sgen, Structures::State& write, int max_depth){
+    bool SMCRun(SMCSuccessorGenerator& sgen, const PetriNet *net, int max_depth){
 
         int current_depth = 0;
         uint32_t max = std::numeric_limits<uint32_t>::max();
         uint32_t tcurrent = 0;
         uint32_t tindex = 0;
+
+
+        Structures::State write(net->makeInitialMarking());
+        sgen.prepare(&write);
+        sgen.reset();
         
+        int n_size = net->numberOfPlaces();
+        std::cout << "size: " << n_size << std::endl;
+        std::cout << "initial marking: " << *write.marking() << "\n" << std::endl;
+
         // Fire transitions until property is satisfied, deadlock, or max depth is reached
-        while (current_depth <= max_depth) { 
-            std::cout << "c_depth: " << current_depth << std::endl;           
+        while (current_depth < max_depth) { 
+            std::cout << "\nc_depth: " << current_depth << std::endl;           
             // Check if state satisfies property here
             if (false) {
                 return true;
@@ -43,30 +52,30 @@ namespace SMC {
 
             int n = 1;
             if (tindex != max) {
-                std::cout << "tindex:" << tindex << ", checkpreset: " << sgen.checkPreset(tindex) << ", write: " << *write.marking() << "\n" << std::endl;
+               std::cout << "tindex:" << tindex << ", checkpreset: " << sgen.checkPreset(tindex) << ", write: " << *write.marking() << std::endl;
             }
             else {
-                std::cout << "tindex:" << tindex << ", write: " << *write.marking() << "\n" << std::endl;
+               std::cout << "tindex:" << tindex << ", write: " << *write.marking() << std::endl;
             }
-            
+
             // Choosing transition to fire with uniform probabilities
             while (sgen.next(write, tindex)) {
-                float randomNum = rand()/RAND_MAX;
-                if (randomNum <= 1./((float)n)) {
+                double randomNum = (double)rand()/RAND_MAX;
+                if (randomNum <= 1./((double)n)) {
+                    std::cout << "current t: " << tcurrent << ", next t: " << tindex << std::endl;
                     tcurrent = tindex;
                 }
                 n++;
             }
-
             // Check if there is a transition to fire
-            if (tcurrent != max) {
+            if (tcurrent != max && tcurrent > 1) {
                 std::cout << "tcurrent != max, tcurrent: " << tcurrent << std::endl;
-                //sgen.fire(write, tcurrent);
-            } else {
-                return false;
+                sgen.fire(write, tcurrent);
+                return true;
             }
 
             current_depth++;
+            sgen.reset();
         }
         return false;
     }
@@ -77,18 +86,10 @@ namespace SMC {
         int total_runs = 0;
         int successful_runs = 0;
         SMCSuccessorGenerator sgen(*net);
-        Structures::State write(net->makeInitialMarking());
-        sgen.prepare(&write);
-
-        int n_size = net->numberOfPlaces();
-        std::cout << "size: " << net->numberOfPlaces() << std::endl;
-
-        std::cout << "initial marking: " << *write.marking() << "\n" << std::endl;
 
         for (int i = 0; i < options.smcruns; i++) {
-            if (SMCRun(sgen, write, options.smcdepth)) {
+            if (SMCRun(sgen, net, options.smcdepth)) {
                 successful_runs++;
-                sgen.reset();
             }
             std::cout << "run " << total_runs << " finished.\n" << std::endl;
             total_runs++;
