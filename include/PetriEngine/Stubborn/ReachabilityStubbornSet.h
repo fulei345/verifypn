364c62e2c0a47a41e392b4009dac91d2bdd7011a
faje18 <faje18@student.aau.dk>
2022-04-21 14:00:37 +0200

    added constructor with single query to ReachabilityStubbornSet

--- include/PetriEngine/Stubborn/ReachabilityStubbornSet.h
+++ include/PetriEngine/Stubborn/ReachabilityStubbornSet.h
@@ -30,6 +30,11 @@ namespace PetriEngine {
             setInterestingVisitor<InterestingTransitionVisitor>();
         }
 
+        ReachabilityStubbornSet(const PetriNet &net, const PQL::Condition_ptr &query, bool closure = true)
+                : StubbornSet(net, query), _closure(closure) {
+            setInterestingVisitor<InterestingTransitionVisitor>();
+        }
+
         ReachabilityStubbornSet(const PetriNet &net, bool closure = true)
                 : StubbornSet(net) , _closure(closure) {
             setInterestingVisitor<InterestingTransitionVisitor>();
