2350f4440c584f66ff4e1171e545074361d95a78
t-bsh <tbsh18@student.aau.dk>
2022-05-30 13:16:17 +0200

    update scripts for new main

--- reproducability/scripts/consistency/consistency.sh
+++ reproducability/scripts/consistency/consistency.sh
@@ -7,13 +7,13 @@ for m in 1 2 3 4 ; do
     for t in bfs dfs heur ; do
         cat ../../tapaalresults/$t\negativeresults >> \true/negativeresults
     done
-    python3 ../main.py --f \true/ -format 4 --m
+    python3 ../main.py --f \true/ -format 3 --i
 
     mkdir \false
     cp ../../heuristicsresults/app$m-false-results \false/
     for t in bfs dfs heur ; do
         cat ../../tapaalresults/$t\positiveresults >> \false/positiveresults
     done
-    python3 ../main.py --f \false/ -format 4 --m
+    python3 ../main.py --f \false/ -format 3 --i
     cd ..
 done
