51bf6a8fd0496b825793baacdb0f4b9e6afd3303
t-bsh <tbsh18@student.aau.dk>
2022-05-30 13:01:46 +0200

    update scripts with new main

--- reproducability/scripts/cleaner.sh
+++ reproducability/scripts/cleaner.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 for i in 1 2 3 4 ; do
-    python3 main.py --f app$i/ -format 4 --s
+    python3 main.py --f app$i/ -format 3 --s
     mv app$i/notime-results .
-    python3 main.py --f app$i/ -format 4 --m
+    python3 main.py --f app$i/ -format 3 --i
     mv notime-results app$i/
-    python3 main.py --f app$i-ft/\true -format 4 --m
-    python3 main.py --f app$i-ft/\true -format 4 --s
+    python3 main.py --f app$i-ft/\true -format 3 --i
+    python3 main.py --f app$i-ft/\true -format 3 --s
 done
